0fcd1fa63c5ad0ba69c385548cfec0255bc63d96
Aquaticat <an@aquati.cat>
2023-02-04 05:02:10 -0500

    build: project direction change...yet again
           This time I'm preparing to separate source Sass into small, small files.
    
    build: using ViteJS.dev as the build system.

---
+++ vite.config.js
@@ -0,0 +1,102 @@
+// Why do I need to write this import statement?
+import * as fs from 'fs';
+import { parse } from 'node-html-parser';
+
+
+export const fixHtmlHead = () => {
+  return {
+    name: 'vite-plugin-fixHtmlHead',
+    closeBundle: async () => {
+      await (() => {
+        const html = parse(fs.readFileSync('docs/index.html', {encoding: 'utf-8'}));
+
+        html.querySelector('[src="/assets/index.js"]').removeAttribute('crossorigin').setAttribute('src', 'assets/index.js');
+        html.querySelector('[href="/assets/style.css"]').setAttribute('blocking', 'render').setAttribute('href', 'assets/style.css');
+        html.querySelector('[href="/monochromatic_icon_1-lightFg_darkBg.svg"]').setAttribute('href', 'monochromatic_icon_1-lightFg_darkBg.svg');
+
+        fs.writeFileSync('docs/index.html', html.toString());
+      })();
+    }
+  };
+};
+
+export default ({
+  //region Shared Options
+  root: 'src',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        OutputStyle: 'expanded',
+      },
+    },
+  },
+  esbuild: {
+    minifyIdentifiers: false,
+    minifySyntax: false,
+  },
+  clearScreen: false,
+  //endregion
+
+  //region Server Options
+  server: {
+    host: true,
+    strictPort: true,
+    open: 'index.html',
+    fs: {
+      strict: false,
+      allow: [
+        'index.html',
+        'M/index.css',
+        'M/index.mjs',
+      ]
+    }
+  },
+  //endregion
+
+  //region Build Options
+  build: {
+    target: 'esnext',
+    modulePreload: false,
+    // For GitHub pages "classic experience".
+    emptyOutDir: true,
+    outDir: '../docs',
+    assetsInlineLimit: 0,
+    cssCodeSplit: false,
+    reportCompressedSize: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+  //endregion
+
+  //region Preview Options
+  preview: {
+    host: true,
+    strictPort: true,
+    open: 'index.html',
+
+  },
+  //endregion
+
+  //region Dep Optimization Options
+  optimizeDeps: {
+    entries: [
+      'index.html'
+    ],
+    force: true,
+  },
+  //endregion
+
+  //region Plugins
+  plugins: [
+    {
+      ...fixHtmlHead(),
+      apply: 'build',
+    }
+  ]
+  //endregion
+});
